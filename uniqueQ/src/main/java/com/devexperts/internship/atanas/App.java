fca8849d5af2efb5b2eb23ab2a61976d98d3aca8
Atanas Dimitrov <adimitrov@devexperts.com>
2023-04-12 16:15:19 +0300

    Maven project skeleton created by quickstart-archetype

---
+++ uniqueQ/src/main/java/com/devexperts/internship/atanas/App.java
@@ -0,0 +1,13 @@
+package com.devexperts.internship.atanas;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
